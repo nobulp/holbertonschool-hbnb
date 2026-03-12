6aef89ec2f5dc1cdfda1dc2833baaf99297dd4c8
nobulp <lucaslupon873@gmail.com>
2026-03-12 11:27:08 +0100

    fix: Restore import of auth namespace in app initialization

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from app.api.v1.auth import api as auth_ns
+
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -19,6 +19,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
